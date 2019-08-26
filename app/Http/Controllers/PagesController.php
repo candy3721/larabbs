707c6d753610cfac20aee8c207187da4f5feadb4
vagrant <vagrant@homestead>
2019-08-26 11:31:49 +0000

    邮箱认证

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
+
 
 class PagesController extends Controller
 {
     public function root()
     {
+        //dd(Auth::user()->hasVerifiedEmail());
+
+
         return view('pages.root');
     }
 }
