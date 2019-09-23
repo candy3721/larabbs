05f56a76c629bf04f92e26d4d6ac4277dcab30ff
vagrant <vagrant@homestead>
2019-09-23 08:39:20 +0000

    20190923

--- routes/web.php
+++ routes/web.php
@@ -36,3 +36,4 @@ Route::get('email/resend', 'Auth\VerificationController@resend')->name('verifica
 Route::resource('users','UsersController',['only' =>['show','update','edit']]);
 
 //Route::get('test','PagesController@test');
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
